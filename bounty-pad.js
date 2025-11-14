2d6db2d7d560f8d0a33d541388ade78c364d03d7
jsavko <james@geekwebservices.com>
2025-11-14 14:35:27 -0500

    Complete rewrite

--- bounty-pad.js
+++ bounty-pad.js
@@ -21,7 +21,7 @@ Hooks.once("init", async function () {
         game.i18n.localize("SETTINGS.bountyPadPermission.GM")
     ];
     
-    game.settings.register("bountyPad", "permissions-emit", {
+    game.settings.register("bounty-pad", "permissions-emit", {
         name: "SETTINGS.bountyPadPermission.Title",
         hint: "SETTINGS.bountyPadPermission.TitleHint",
         scope: "world",
@@ -38,7 +38,7 @@ Hooks.once("init", async function () {
 
 
 Hooks.on('getActorContextOptions', (html, options)=>{
-    if ( game.user.role >= game.settings.get("bountyPad", "permissions-emit")) {
+    if ( game.user.role >= game.settings.get("bounty-pad", "permissions-emit")) {
         options.push(
             {
               "name": `Display DataPad`,
@@ -53,11 +53,43 @@ Hooks.on('getActorContextOptions', (html, options)=>{
     }
 });
 
+Hooks.on("renderActorSheet", (app, [html], data) => {       
+    
+    if ( game.user.role <= game.settings.get("bounty-pad", "permissions-emit")) return 
+
+
+  const nav = html.querySelector(".fatex-tabs-navigation"); //chekc this selector
+  const body = html.querySelector(".fatex-js-tab-content"); //check this selector 
+  console.log(html);
+  if(!nav || !body) return;
+
+  console.log('found tabs')
+  
+  nav.insertAdjacentHTML('beforeend',
+    `<a class="fatex-tabs-navigation__item"... data-tab="bounty">Bounty Data</a>`
+  );
+
+let bountryImg = foundry.applications.elements.HTMLFilePickerElement.create({
+    name: "system.flags.bounty.img",
+    type: "image",
+    value: app.object.system.flags?.bounty.img
+  })
+let statProse= foundry.applications.elements.HTMLProseMirrorElement.create({name:"system.flags.bounty.stats",toggled: true,value: app.object.system.flags?.bounty.stats, enriched:app.object.system.flags?.bounty.stats});
+let detailsProse= foundry.applications.elements.HTMLProseMirrorElement.create({name:"system.flags.bounty.details",toggled: true,value: app.object.system.flags?.bounty.details, enriched:app.object.system.flags?.bounty.details});
+
+  body.insertAdjacentHTML('beforeend', `
+    <div data-tab="bounty" class="fatex-tab-content fatex-tab-content--bounty tab">
+       <h2>Bounty Image</h2><div>${bountryImg.outerHTML}</div>
+       <h2>Bounty Stats</h2><div> ${statProse.outerHTML}</div>
+       <h2>Bounty Details</h2> <div>${detailsProse.outerHTML}</div>
+    </div>
+  `);
+});
 
     async function displayPad(options={}) {
         //if (!game.user.isGM) {
 
-        if ( game.user.role <= game.settings.get("bountyPad", "permissions-emit")) { 
+        if ( game.user.role <= game.settings.get("bounty-pad", "permissions-emit")) { 
             ui.notifications.warn(game.i18n.localize("bountyPad.ErrorGM"));
             return;
         }
@@ -146,12 +178,7 @@ class bountyPadOverlay extends HandlebarsApplicationMixin(ApplicationV2) {
    async _prepareContext(options) {
     const context = await super._prepareContext(options);
     context.actor = game.actors.get(this.options.actor);
-    const bounty = {
-    stats: getSection("Bounty|Info|Stats", context.actor.system.biography.value),
-    details: getSection("Bounty|Info|Details", context.actor.system.biography.value)
-    };
-    //bounty.details = stats;
-    context.bounty = bounty;
+    context.bounty = context.actor.system.flags.bounty;
     return context
   }
 
